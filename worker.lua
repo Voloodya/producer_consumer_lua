5d90239fa1eb0fa83356eeda918fb8f5120f56f9
Cloud User <centos@blaze.novalocal>
2021-12-20 16:19:08 +0300

    First commit

---
+++ worker.lua
@@ -0,0 +1,40 @@
+box.cfg{ listen = '127.0.0.1:3301' }
+box.schema.user.grant('guest','super', nil, nil, {if_not_exists=true})
+
+-- Create module fiber
+fiber = require 'fiber'
+-- Create канала. Переменная не local -> глобальная - доступна из консоли
+chan = fiber.channel()
+
+-- "..." - список аргументов
+function put_msg(...)
+    chan:put({...}) -- {...} - преобразование в таблицу
+end
+
+--
+function get_msg(timeout)
+    local msg = chan:get(timeout or 1)
+    if not msg then error("No messages") end
+    return msg
+end
+
+-- Создание нового fiber-а
+fiber.create(function()
+    fiber.name('worker')
+
+    -- Получение сообщения из канала в бесконечном цикле
+    while true do
+        local msg = chan:get_msg(1)
+        if msg then
+            print("Got a message: ",msg)
+        end
+    end
+end)
+
+--Запуск консоли
+require('console').start()
+--Завершение процесса после закрытия консоли
+os.exit()
+
+--tarantool worker.lua
+--chan:put("message")
